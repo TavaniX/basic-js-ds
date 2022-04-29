38b4495691335847c1a84a1c973d42b38110029c
Maksim Aisin <maksim.aisin@mail.ru>
2022-04-29 23:27:25 +0300

    feat: queue - enqueue

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,32 @@ const { ListNode } = require('../extensions/list-node.js')
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-    getUnderlyingList() {
-        throw new NotImplementedError('Not implemented')
-        // remove line with error and write your code here
+    constructor() {
+        this.head = 0
+        this.length = 0
     }
 
-    enqueue(/* value */) {
-        throw new NotImplementedError('Not implemented')
-        // remove line with error and write your code here
+    getUnderlyingList() {}
+
+    enqueue(value) {
+        let node = new ListNode(value)
+
+        if (this.length === 0) {
+            this.head = node
+        } else {
+            let current = this.head
+
+            while (current.next) {
+                current = current.next
+            }
+
+            current.next = node
+        }
+
+        this.length++
     }
 
-    dequeue() {
-        throw new NotImplementedError('Not implemented')
-        // remove line with error and write your code here
-    }
+    dequeue() {}
 }
 
 module.exports = {
