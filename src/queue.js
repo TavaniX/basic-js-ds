8f2bf656d1e767d0da8ae320f49ba28a6ff5961c
Maksim Aisin <maksim.aisin@mail.ru>
2022-04-29 23:51:40 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js')
+// const { NotImplementedError } = require('../extensions/index.js')
 
-const { ListNode } = require('../extensions/list-node.js')
+// const { ListNode } = require('../extensions/list-node.js')
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,13 +13,23 @@ const { ListNode } = require('../extensions/list-node.js')
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+    constructor(x) {
+        this.value = x
+        this.next = null
+    }
+}
+
 class Queue {
     constructor() {
         this.head = 0
         this.length = 0
     }
 
-    getUnderlyingList() {}
+    getUnderlyingList() {
+        return this
+    }
 
     enqueue(value) {
         let node = new ListNode(value)
@@ -39,9 +49,25 @@ class Queue {
         this.length++
     }
 
-    dequeue() {}
+    dequeue() {
+        let current = this.head
+        this.head = current.next
+        this.length--
+    }
 }
 
-module.exports = {
-    Queue,
-}
+let test = new Queue()
+test.enqueue('first')
+console.log(test)
+test.enqueue(2)
+console.log(test)
+test.enqueue(342)
+console.log(test)
+console.log(test.getUnderlyingList())
+test.dequeue()
+console.log('dequeued: ', test)
+console.log(test.getUnderlyingList())
+
+// module.exports = {
+//     Queue,
+// }
